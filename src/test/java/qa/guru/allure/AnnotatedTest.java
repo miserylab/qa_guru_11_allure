0cf6680cf828d722b4d474a86d6d285041f24411
Olga Demina <misertylab.r6s@gmail.com>
2023-07-18 15:43:57 +0300

    init commit

---
+++ src/test/java/qa/guru/allure/AnnotatedTest.java
@@ -0,0 +1,22 @@
+package qa.guru.allure;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+public class AnnotatedTest {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE =  80;
+    @Test
+    public void testAnnotatedStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.clickOnRepositoryLink(REPOSITORY);
+        steps.openIssuesTab();
+        steps.shouldSeeIssueWithNumber(ISSUE);
+    }
+}
---
+++ src/test/java/qa/guru/allure/AttachmentsTest.java
@@ -0,0 +1,37 @@
+package qa.guru.allure;
+
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
+
+public class AttachmentsTest {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE =  80;
+
+    @Test
+    public void testLambdaStepAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+            attachment("Source", webdriver().driver().source());
+        });
+    }
+
+    @Test
+    public void testAnnotatedStepAttachments() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+
+        steps.openMainPage();
+        steps.takeScreenshot();
+
+    }
+}
